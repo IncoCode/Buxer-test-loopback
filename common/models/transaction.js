7f02b30e21e467a92fc399ce2118e1ada223fd2c
IncoCode <akulich.dimon@gmail.com>
2015-07-09 14:00:42 +0300

    Added Transaction model validating

--- common/models/transaction.js
+++ common/models/transaction.js
@@ -1,3 +1,8 @@
-module.exports = function(Transaction) {
+module.exports = function (Transaction) {
+  function isAmountValid(err) {
+    if (this.Amount < 1) err();
+  }
 
+  Transaction.validatesInclusionOf('Type', {in: ['income', 'expense']});
+  Transaction.validate('Amount', isAmountValid, {message: 'Amount should be greater zero!'});
 };
