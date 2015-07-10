931ba97f762884be48fdd9e41ea9f565c868ce0b
IncoCode <akulich.dimon@gmail.com>
2015-07-10 14:00:30 +0300

    Added getUserBudget remote method (returns Budget info for specified user)

--- server/boot/script.js
+++ server/boot/script.js
@@ -1,32 +1,85 @@
 module.exports = function (app) {
+  var Promise = require('bluebird');
+  var _ = require('underscore');
+
   var Transaction = app.models.Transaction;
+  var Account = app.models.Account;
   var Budget = app.models.Budget;
 
-  var bFindById = Budget.findById;
-  Budget.findById = function (id, filter, cb) {
+  Budget.getUserBudget = function (userId, startDate, endDate, cb) {
+    if (!userId || !startDate || !endDate)
+      cb("Wrong params!");
 
-    var budgetObj;
+    var budget;
+    var startDateObj = new Date(startDate);
+    var endDateObj = new Date(endDate);
 
-    bFindById
-      .call(Budget, id)
+    Budget
+      .findOne({where: {myUserId: userId}})
       .then(function (b) {
+        if (!b)
+          return Promise.reject("User not found or Budget for this user has not been created!");
 
-        budgetObj = b;
-        return Transaction.find();
+        budget = b;
+        budget.Limit = Budget.getLimitForPeriod(budget, daysBetween(startDateObj, endDateObj));
+
+        return Account.find({
+          include: 'transactions',
+          where: {
+            myUserId: budget.myUserId
+          }
+        })
       })
-      .then(function (transactions) {
+      .then(function (accounts) {
+        return Promise.map(accounts, function (acc) {
+          return Transaction
+            .find({
+              where: {
+                and: [
+                  {Date: {gte: startDateObj}},
+                  {Date: {lte: endDateObj}}
+                ],
+                accountId: acc.id
+              }
+            })
+        });
+      })
+      .then(function (transactionsArr) {
+        var transactions = _.flatten(transactionsArr);
 
         var expense = transactions.reduce(function (prevVal, currVal) {
-
           return prevVal + (currVal.Type === 'expense' ? currVal.Amount : 0);
         }, 0);
 
-        budgetObj.Balance = budgetObj.SpendingLimit - expense;
-        budgetObj.Expense = expense;
+        budget.Balance = (budget.Limit - expense).toFixed(2);
+        budget.Expense = expense;
 
-        cb(null, budgetObj);
+        cb(null, budget);
       })
       .catch(cb)
+  };
+  Budget.remoteMethod(
+    'getUserBudget',
+    {
+      accepts: [{arg: 'userId', type: 'string'}, {arg: 'startDate', type: 'string'}, {arg: 'endDate', type: 'string'}],
+      returns: {arg: 'Budget', type: 'object'},
+      http: {path: '/getUserBudget', verb: 'get'}
+    }
+  );
+
+  function daysBetween(date1, date2) {
+    // get 1 day in milliseconds
+    var one_day = 1000 * 60 * 60 * 24;
+
+    // convert both dates to milliseconds
+    var date1_ms = date1.getTime();
+    var date2_ms = date2.getTime();
+
+    // calculate the difference in milliseconds
+    var difference_ms = date2_ms - date1_ms;
+
+    // convert back to days and return
+    return Math.round(difference_ms / one_day);
   }
 
 };
