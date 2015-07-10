d1f4706bb446e28cdd9af771466834733df6ca90
IncoCode <akulich.dimon@gmail.com>
2015-07-10 13:56:35 +0300

    Added Budget.getLimitForPeriod function

--- common/models/budget.js
+++ common/models/budget.js
@@ -1,3 +1,20 @@
-module.exports = function(Budget) {
+module.exports = function (Budget) {
+  Budget.getLimitForPeriod = function (budgetObj, daysPeriod) {
+    var dayStep = 0;
+    switch (budgetObj.Period) {
+      case 'week':
+        dayStep = budgetObj.SpendingLimit / 7;
+        break;
+
+      case 'month':
+        dayStep = budgetObj.SpendingLimit / 30;
+        break;
+
+      case 'year':
+        dayStep = budgetObj.SpendingLimit / 360;
+        break;
+    }
+    return (dayStep * daysPeriod).toFixed(2);
+  }
 
 };
